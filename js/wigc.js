ea802a5ff80e2ebb16ea4ee4e92f899d6c85282e
Lemon <AhoyLemon@users.noreply.github.com>
2017-12-12 16:22:21 -0600

    Early defaults.

---
+++ js/wigc.js
@@ -0,0 +1,14 @@
+// jshint -W117
+// jshint -W119
+
+//@prepros-prepend partials/_functions.js
+
+var app = new Vue({
+  el: '#app',
+  data: {
+   // Nothing yet.
+  },
+  computed: {
+    // Nothing yet
+  }
+});
