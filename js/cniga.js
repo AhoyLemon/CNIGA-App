2ebde59050d058c5bc55ab3598e6fb43b0ecaef0
Lemon <AhoyLemon@users.noreply.github.com>
2018-02-02 15:04:27 -0600

    renamed icon to white, knock wood.

--- js/cniga.js
+++ js/cniga.js
@@ -153,7 +153,7 @@ var app = new Vue({
       console.log('calling push init');
       var push = PushNotification.init({
         "android": {
-          "icon": "ic_stat_onesignal_default",
+          "icon": "white",
           "iconColor": "#f26c51",
           "senderID": "1098348384269"
         },
