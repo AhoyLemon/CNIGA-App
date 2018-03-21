0f69d4286d232372958bd7f7b5c46b20ed3ee80d
Lemon <AhoyLemon@users.noreply.github.com>
2018-03-21 11:16:06 -0500

    iFrame load/unload

--- js/cniga_android.js
+++ js/cniga_android.js
@@ -7,8 +7,6 @@
 
 function onLoad() {
   document.addEventListener("deviceready", onDeviceReady, false);
-  new Audio('audio/ready.mp3').play();
-  alert('this is version 5! (full system, but with alerts)');
 }
 
 onLoad();
@@ -22,16 +20,10 @@ function onDeviceReady() {
 // Handle the pause event
 //
 function onPause() {
-  //new Audio('audio/ready.mp3').play();
-  alert('on pause event start');
   document.getElementById("iframe").src = "about:blank";
-  alert('on pause event end');
 }
 
 
 function onResume() {
-  //new Audio('audio/ready.mp3').play();
-  alert('on resume event start');
   document.getElementById("iframe").src = "https://cniga.com";
-  alert('on resume event end');
 }
