d430138832976406a1f7ec24910765a114d7aaf3
Lemon <AhoyLemon@users.noreply.github.com>
2018-03-21 11:07:48 -0500

    v5

--- js/cniga_android.js
+++ js/cniga_android.js
@@ -8,17 +8,15 @@
 function onLoad() {
   document.addEventListener("deviceready", onDeviceReady, false);
   new Audio('audio/ready.mp3').play();
-  alert('this is version 4');
+  alert('this is version 5! (full system, but with alerts)');
 }
 
 onLoad();
 
-//var iFrameAndroid = document.getElementById("#iFrameAndroid"); 
-
 // Cordova is loaded and it is now safe to make calls Cordova methods
-//
 function onDeviceReady() {
   document.addEventListener("pause", onPause, false);
+  document.addEventListener("resume", onResume, false);
 }
 
 // Handle the pause event
@@ -26,6 +24,14 @@ function onDeviceReady() {
 function onPause() {
   //new Audio('audio/ready.mp3').play();
   alert('on pause event start');
-  document.getElementById("iframe").src = "https://redcircleagency.com";
+  document.getElementById("iframe").src = "about:blank";
   alert('on pause event end');
+}
+
+
+function onResume() {
+  //new Audio('audio/ready.mp3').play();
+  alert('on resume event start');
+  document.getElementById("iframe").src = "https://cniga.com";
+  alert('on resume event end');
 }
